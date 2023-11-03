a20613c7173ffb49b3de6fbb820ddf96a6120f3d
nuray <nurayekmekcim@gmail.com>
2023-11-03 15:56:10 +0100

    first commit

---
+++ src/pojeDeneme/ProjeDeneme.java
@@ -0,0 +1,7 @@
+package pojeDeneme;
+
+public class ProjeDeneme {
+    public static void main(String[] args) {
+        System.out.println("Deneme");
+    }
+}
---
+++ src/projeDeneme2/Deneme2.java
@@ -0,0 +1,7 @@
+package projeDeneme2;
+
+public class Deneme2 {
+    public static void main(String[] args) {
+        System.out.println("Deneme");
+    }
+}
